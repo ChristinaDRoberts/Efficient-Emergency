caa0576ffc05ab7338686389a8d131e982a8ff89
Christina <Christinepeninsula@hotmail.com>
2019-03-11 12:22:15 -0400

    html for dispatch and client

--- frontend/models.py
+++ frontend/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 
 class Client(models.Model):
-    image = models.FileField(blank=True, upload_to='media')
-    descriptiom = models.CharField(max_length=255, null=True)
+    image = models.FileField(blank=True, upload_to='')
+    # description = models.CharField(max_length=255, null=True)
---
+++ frontend/static/src/containers/App.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react';
+
+import '../App.css';
+import {Container, Form, Button} from 'react-bootstrap';
+
+
+
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            image_preview: "",
+            image: "",
+            imageCollection: []
+    };
+        this.handleImage = this.handleImage.bind(this);
+        this.submitImage = this.submitImage.bind(this);
+        this.addImageToArray = this.addImageToArray.bind(this);
+
+    }
+
+  handleImage(event) {
+        //sets the preview box of image in react element
+        event.preventDefault();
+
+        let file = event.target.files[0];
+        let fileReader = new FileReader();
+        fileReader.onloadend = () => this.setState({image_preview: fileReader.result});
+        fileReader.readAsDataURL(file);
+        this.setState({image: file});
+    }
+
+    addImageToArray(image) {
+        //adds image to the state of imageCollection Array
+        let imageCollection = [...this.state.imageCollection];
+        imageCollection.push(image);
+        this.setState({imageCollection});
+        console.log(this.state.imageCollection)
+    }
+
+    submitImage(e) {
+        e.preventDefault();
+         const conf = {
+                method: "POST",
+                body: "application/json",
+
+            };
+
+
+        fetch('/api/scene/', conf).then((response) => {
+            return response.json();
+        }).then((json) => {
+            this.addImageToArray(json);
+            console.log("added")
+
+
+        }).then((data)=> {
+            console.log("data", data);
+            this.setState({imageCollection: data});
+            console.log(this.state.imageCollection)
+         });
+
+
+
+    }
+
+
+  render(){
+    return(
+
+        <div>
+          <h1>Testing Image </h1>
+
+     <Form onSubmit={this.submitImage}>
+        <Container>
+          <img src={this.state.image_preview}/>
+          <input className="input" type="file" onChange={this.handleImage} name="image"/>
+
+
+             <Button className="addIngredientButton" type="submit" variant="secondary">Submit This Image !</Button>
+        </Container>
+
+     </Form>
+
+        </div>
+    )
+  };
+
+}
+
+
+export default App;
+
+
+
+
+
+

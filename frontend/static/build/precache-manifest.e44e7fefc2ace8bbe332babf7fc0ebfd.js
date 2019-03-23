b2d76ec7a7f48c84d9cba2c3b67ff87d7fb096ad
Christina <Christinepeninsula@hotmail.com>
2019-03-23 17:03:01 -0400

    took off card borders, solved jumping around problem, going to make modal work to avoid overlapping issues

---
+++ frontend/static/build/precache-manifest.e44e7fefc2ace8bbe332babf7fc0ebfd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "48987b25ef511ff8d3b6",
+    "url": "/static/css/main.62f70a6c.chunk.css"
+  },
+  {
+    "revision": "48987b25ef511ff8d3b6",
+    "url": "/static/js/main.41fd0b1a.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "f42fa965e55a3401d161",
+    "url": "/static/js/2.2b0b479e.chunk.js"
+  },
+  {
+    "revision": "4a1d5651192418d47745dbf64920d25c",
+    "url": "/static/media/clientbackground.4a1d5651.jpg"
+  },
+  {
+    "revision": "2076cd5927935054d6dff29470e6c22f",
+    "url": "/index.html"
+  }
+];
--- frontend/static/build/service-worker.js
+++ frontend/static/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.26509aa353fe32aff1783b5b39124fac.js"
+  "/precache-manifest.e44e7fefc2ace8bbe332babf7fc0ebfd.js"
 );
 
 workbox.clientsClaim();
